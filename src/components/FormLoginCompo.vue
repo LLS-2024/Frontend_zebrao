2f849122444f3a4489138ea44c49635880f390d5
Luizaescarban <luizaescarbandasil@gmail.com>
2025-06-13 08:11:36 -0300

    FEAT: Finalização formulário

--- src/components/FormLoginCompo.vue
+++ src/components/FormLoginCompo.vue
@@ -1,24 +1,30 @@
 <script setup>
-  import { ref } from 'vue';
+  import { ref } from 'vue'
 
-  const nome = ref('');
+const dados = ref({
+  nome: '',
+  senha: '',
+  email: '',
+})
 </script>
 
 <template>
     <section>
     <div class="dados"> <div class="nome"><p><label for="nome">Nome</label> </p>
-    <input type="text" v-model="nome" id="nome" /></div>
+    <input type="text" v-model="dados.nome" id="nome" /></div>
     <div class="email">
     <p><label for="email">Email</label></p>
-    <input type="email" v-model="email" id="email"/>
+    <input type="email" v-model="dados.email" id="email"/>
     </div>
     
     </div>
    <div class="senha">
    <p><label for="senha">Senha</label></p>
-   <input type="password" id="senha" name="senha">
+   <input type="password" v-model="dados.senha" id="senha" name="senha">
    </div>
-   <div></div>
+   <div>
+   <button @click="enviar" type="submit">Entrar</button>
+   </div>
     </section>
 </template>
 <style scoped>
